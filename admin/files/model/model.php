027858a495e116f12fc077b8e6d4007ee2796082
IHCBorja <36751846+IHCBorja@users.noreply.github.com>
2019-10-17 01:41:33 +0800

    add dash

--- admin/files/model/model.php
+++ admin/files/model/model.php
@@ -29,7 +29,7 @@
                   $img = $_FILES['image']['name'];
                   $tmp = $_FILES['image']['tmp_name'];
                   $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-                  $final_image = rand(1000,1000000).$img;
+                  $final_image = rand(1000,1000000).'-'.$img;
                     if(in_array($ext, $valid_extensions)) { 
                       $path = $path.strtolower($final_image); 
                         if(move_uploaded_file($tmp,$path)) {
